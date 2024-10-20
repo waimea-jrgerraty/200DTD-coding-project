10cdd267338c4244717476bd1abbfeec1008d116
waimea-jrgerraty <jrgerraty@waimea.school.nz>
2024-10-21 10:55:53 +1300

    GameState

--- src/Main.kt
+++ src/Main.kt
@@ -12,12 +12,32 @@
  */
 
 import kolour.*
+import gameState.GameState as GameState
+
+fun printState(game: GameState) {
+    // test print coin positions
+    repeat(game.rows.toInt()) { i ->
+        print("${i + 1}:${game.getCoin(i)} ")
+    }
+}
+
 
 /**
  * Program entry point
  */
 fun main() {
     println("Hello World!")
-    colourTest()
+//    colourTest()
+    val game = GameState(16u, 4u)
+    // test print coin positions
+    printState(game)
+    println("")
+    print("Chose a position to push: ")
+    val pos = readln().toIntOrNull()
+    if (pos != null) {
+        val moveState = game.pushCoin(pos)
+        println(moveState)
+        printState(game)
+    }
 }
 
--- src/gameState/GameState.kt
+++ src/gameState/GameState.kt
@@ -2,6 +2,10 @@ package gameState
 
 import kotlin.random.Random
 
+enum class MoveState {
+    WIN, VALID, INVALID
+}
+
 class GameState(rows: UByte, coins: UByte) {
     // Clamp the rows to sensible values for gameplay purposes
     val rows = rows.coerceIn(8u, 32u)
@@ -17,7 +21,7 @@ class GameState(rows: UByte, coins: UByte) {
         val randomNumbers = mutableSetOf<UByte>()
 
         while (randomNumbers.size.toUByte() < clampedCoins) {
-            randomNumbers.add(Random.nextInt(2, rows.toInt() + 1).toUByte())
+            randomNumbers.add(Random.nextInt(0, rows.toInt()).toUByte())
         }
 
         // Pick one of these random values to use as the gold coin
@@ -31,5 +35,36 @@ class GameState(rows: UByte, coins: UByte) {
         coinData[gold] = true
     }
 
-    
+    fun getCoin(position: Int): Boolean? {
+        return coinData[(position - 1).toUByte()]
+    }
+
+    // Position is 1 to rows, instead of 0 to rows - 1
+    fun pushCoin(position: Int): MoveState {
+        val coin = getCoin(position - 1)
+        if (coin != null) {
+            // Handle win state and coin removal
+            if (position == 1) {
+                coinData[0u] = null
+                return if (coin == true) {
+                    MoveState.WIN
+                } else {
+                    MoveState.VALID
+                }
+            }
+            // Try to push coin to the left
+            // If there is a coin to the left we want it to return invalid
+            if (getCoin(position - 2) == null) {
+                // Push the coin as far to the left as possible
+                var i = position - 1
+                while (i > 1 && getCoin(i - 1) == null) {
+                    i--
+                }
+                coinData[i.toUByte()] = coin
+                coinData[(position - 1).toUByte()] = null
+                return MoveState.VALID
+            }
+        }
+        return MoveState.INVALID
+    }
 }
