f4df19272f572290c14694aa06bb5a935f45c50c
waimea-jrgerraty <jrgerraty@waimea.school.nz>
2024-10-26 16:03:10 +1300

    display cell number

--- src/display/Display.kt
+++ src/display/Display.kt
@@ -2,6 +2,7 @@ package display
 
 import kolour.*
 import gameState.GameState
+import java.awt.Taskbar
 
 // Instructions to draw the coins in the grid
 // For each line, it will be a map that gives instructions for each type of coin
@@ -28,16 +29,32 @@ fun getCoinInstruction(row: Int, type: Boolean?): String {
 
 fun buildTable(state: GameState) {
     // Build table header
-    println("┌${"────────────┬".repeat(state.rows.toInt() - 1)}────────────┐")
+    val header = StringBuilder() // Using string builder for faster concat
+    header.append("┌")
+
+    // Loop through the number of rows to build each header cell
+    repeat(state.rows.toInt()) { c ->
+        val slotNumber = (c + 1).toString().bold()
+        header.append("─$slotNumber")
+        // Since we have the bold escape code applied, we need to factor in the extra 8 characters
+        header.append("${"─".repeat(11 - (slotNumber.length - 8))}┬")
+    }
+
+    // Remove the last '┬' and add the closing corner
+    header.setLength(header.length - 1)
+    header.append("┐")
+    println(header.toString())
+
     // Build the coins line by line
     repeat(5) { i ->
-        val row = StringBuilder() // Using string builder for faster concat
+        val row = StringBuilder()
 
         repeat(state.rows.toInt()) { c ->
             row.append(getCoinInstruction(i, state.getCoin(c + 1)) + "│")
         }
         println("│${row}")
     }
+
     // Make the bottom border
     println("└${"────────────┴".repeat(state.rows.toInt() - 1)}────────────┘")
 }
